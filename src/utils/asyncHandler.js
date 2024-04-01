0e5b0d59cbc1b2d17f6e73377576e18a86fd06d2
tusharawat01 <tushar0402tr@gmail.com>
2024-04-01 12:58:52 +0530

    Utils, CORS & cookieParser

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((err) => next(err));
+    }
+}
+
+
+
+
+// const asyncHandler = (requestHandler) = async (req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             sucess: true,
+//             message: error.message
+//         })
+//     }
+
+// }
+
+export { asyncHandler };
