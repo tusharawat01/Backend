dab4329cafaec71a53735d4dea3a40388e9cece0
tusharawat01 <tushar0402tr@gmail.com>
2024-04-05 14:47:00 +0530

    Done all Configuration

--- src/app.js
+++ src/app.js
@@ -10,14 +10,22 @@ app.use(cors({
 }))
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
 }));
 
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+// https://localhost8080/api/v1/users/register   
+
+
 
 export { app }
